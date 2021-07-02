7aead67ddbc463a73c33b30b004b07da39803bf0
ShriyaChandran <62100109+ShriyaChandran@users.noreply.github.com>
2021-07-02 21:07:46 +0530

    Add files via upload

---
+++ screens/InputScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {TouchableOpacity, View, StyleSheet, TextInput} from 'react-native';
+
+export default class InputScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Input Screen
+                </Text>
+            </View>
+        )
+    }
+}
---
+++ screens/ReceiverDetails.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {TouchableOpacity, View, StyleSheet, TextInput} from 'react-native';
+
+export default class ReceiverDetails extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Receiving details screen
+                </Text>
+            </View>
+        )
+    }
+}
